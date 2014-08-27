a430e68aa5309877cd5f054d30b91892a87312fb
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-08-27 20:50:24 -0300

    Smarter FactoryGirl snippets

--- rspec-mode.el
+++ rspec-mode.el
@@ -192,6 +192,13 @@ Not used when running specs using Zeus or Spring."
   :type 'string
   :group 'rspec-mode)
 
+(defcustom rspec-snippets-fg-syntax 1
+  "When 0, use full syntax for FactoryGirl snippets. When 1, use full syntax if
+FactoryGirl::Syntax::Methods is not included in the spec_helper file, and uses the concise syntax
+otherwise. When 2, use the concise syntax."
+  :type 'boolean
+  :group 'rspec-mode)
+
 ;;;###autoload
 (define-minor-mode rspec-mode
   "Minor mode for RSpec files"
@@ -701,6 +708,25 @@ or a cons (FILE . LINE), to run one example."
           ((file-exists-p (expand-file-name "Rakefile" directory)) directory)
           ((file-exists-p (expand-file-name "Gemfile" directory)) directory)
           (t (rspec-project-root (file-name-directory (directory-file-name directory)))))))
+
+(defun rspec--include-fg-syntax-methods-p ()
+  "Check wether FactoryGirl::Syntax::Methods is included in spec_helper"
+  (with-temp-buffer
+    (let* ((root-path (rspec-project-root))
+           (spec-helper-path (concat root-path "spec/spec_helper.rb"))
+           (uses-factory-girl-regexp "include +FactoryGirl::Syntax::Methods"))
+      (with-temp-buffer
+        (insert-file-contents spec-helper-path)
+        (cond
+         ((= 0 rspec-snippets-fg-syntax) nil)
+         ((= 1 rspec-snippets-fg-syntax) (re-search-forward uses-factory-girl-regexp nil t))
+         ((= 2 rspec-snippets-fg-syntax) t))))))
+
+(defun rspec-snippets-fg-method-prefix (method)
+  "Return FactoryGirl method for snippet aware of FactoryGirl::Syntax::Methods inclusion in the spec_helper file."
+  (if (rspec--include-fg-syntax-methods-p)
+      method
+    (concat "FactoryGirl." method)))
 
 ;;;###autoload
 (defun rspec-enable-appropriate-mode ()
